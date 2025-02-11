d46783869a48b6459411cb9f95395404265ae68f
uday <udaysubba2004@gmail.com>
2025-02-11 21:09:07 +0530

    add Backend API URL

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
-export const AppUrl = "https://magicminute.online/technical-solution/"
+export const API_URL = "https://magicminute.online/technical-solution"
+
+//dev api url
+// export const API_URL = "http://localhost:8000"
