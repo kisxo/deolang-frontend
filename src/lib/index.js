10d23aa3b1766456b9eb045d6405f8b4f0d385b6
uday <udaysubba2004@gmail.com>
2025-02-12 01:59:13 +0530

    add AppWrite project Id and api end point url

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
 export const API_URL = "https://magicminute.online/technical-solution"
 
-//dev api url
+//dev fastapi api url
 // export const API_URL = "http://localhost:8000"
+
+//AppWrite Client ID
+export const APPWRITE_PROJECT_ID = '67a865bb0036c22b5f5c'
+export const APPWRITE_API_ENDPOINT = 'https://cloud.appwrite.io/v1'
