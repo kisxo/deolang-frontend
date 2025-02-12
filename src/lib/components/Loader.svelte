e712b7e2291d17c8c795d3dab151503efd8ea4ce
uday <udaysubba2004@gmail.com>
2025-02-12 19:54:33 +0530

    add css loader

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,30 @@
+<svg class="container">
+    <rect class="boxes"></rect>
+</svg>
+
+<style>
+    .container {
+        width: 50px;
+        height: 50px;
+        margin-inline: auto;
+        margin-block: 3rem;
+    }
+
+    .container .boxes {
+        width: 50px;
+        height: 50px;
+        fill: none;
+        stroke-width: 50px;
+        stroke: var(--primary-600);
+        stroke-dasharray: 50;
+        stroke-dashoffset: 50%;
+        animation: animate 2s linear infinite;
+    }
+
+    @keyframes animate {
+        to {
+            stroke-dashoffset: 250%;
+        }
+    }
+
+</style>
