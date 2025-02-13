6d0f71611a67a5665133918be2b4b71f8c5b08d4
uday <udaysubba2004@gmail.com>
2025-02-13 14:01:28 +0530

    add delete feature

--- src/routes/admin/dashboard/AdminProductList.svelte
+++ src/routes/admin/dashboard/AdminProductList.svelte
@@ -5,18 +5,72 @@
 
     import axios from "axios";
     import Loader from "$lib/components/Loader.svelte";
+    import Modal from "$lib/components/Modal.svelte";
 
+    let loader_toggle = $state(false)
     let products = $state()
+    let modal_data = $state();
+    function showModal(message, cancel, redirect){
+        modal_data = {
+            "toggle": true,
+            "message": message,
+            "cancel": cancel,
+            "redirect": redirect,
+        }
+    }
 
-    axios.get(API_URL + '/api/products/')
-    .then(function (response) {
-        products = response.data.data
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
+    async function loadProducts() {
+        axios.get(API_URL + '/api/products/')
+        .then(function (response) {
+            products = response.data.data
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    loadProducts()
+
+    async function deleteProduct(productId) {
+        const shouldDelete = window.confirm("Are you sure to delete?");
+        if (!shouldDelete)
+        {
+            return
+        }
+
+        loader_toggle = true
+
+        let options = {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            withCredentials: true,
+        };
+
+        axios(API_URL + '/api/products/' + productId, options)
+        .then(function (response) {
+            if(response.status === 204)
+            {
+                showModal("Deleted product successfully...!", "Continue")
+                loadProducts()
+            }
+        })
+        .catch(function (error) {
+            showModal(error.response.data.detail, "Try Again")
+        })
+        .finally(function () {
+            loader_toggle = false
+        })
+    }
+
 </script>
 
+{#if loader_toggle}
+<div class="delete-loader">
+    <Loader/>
+</div>
+{/if}
 {#if products}
     <table>
         <thead >
@@ -36,10 +90,10 @@
                 </td>
                 <td class="price ">Rs. {product['price']}</td>
                 <td class="action text-white">
-                    <button class="bg-blue-300 px-4 py-1 rounded-xs m-1">
+                    <button class="bg-blue-300 px-6 py-2 rounded-xs m-1">
                         <Pencil/>
                     </button>
-                    <button class="bg-red-600 px-4 py-1 rounded-xs m-1">
+                    <button class="bg-red-600 px-6 py-2 rounded-xs m-1" onclick={ () => { deleteProduct(product['$id'])}}>
                         <Trash/>
                     </button>
                 </td>
@@ -51,8 +105,11 @@
     <Loader/>
 {/if}
 
+<Modal {...modal_data}/>
+
 <style>
     table{
+        position: relative;
         width: 100%;
         padding: 1rem;
         border: 1px solid hsl(0, 1%, 85%);    
@@ -82,6 +139,10 @@
 
     .action{
         text-align: center;
+
+        button:active{
+            transform: scale(.95);
+        }
     }
 
 
@@ -104,4 +165,15 @@
 
     }
 
+    .delete-loader{
+        position: absolute;
+        right: calc(50% - 25px - 5rem);
+        top: 42%;
+        z-index: 99;
+        border-radius: .3rem;
+        border: 1px solid rgb(209, 209, 209);
+        background-color: #fff;
+        padding-inline: 5rem;
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    }
 </style>
